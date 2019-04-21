7d31de02e3ff12d43b622a9456a7238e30d68c3f
Breno Bastos <brenodead@gmail.com>
2019-04-21 17:41:28 -0300

    Teste Commit github

--- src/Course.java
+++ src/Course.java
@@ -1,6 +1,9 @@
 
 public class Course {
     public static void main(String[] args) {
+        System.out.println("Boa dia");
+        System.out.println("Boa tarde");
+        System.out.println("Boa noite");
         
     }
     
